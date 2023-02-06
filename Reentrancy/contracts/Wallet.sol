dd6207a9ba5ea9451c86a797261ce70d93e4338c
Haripandey21 <iamgoing1214@gmail.com>
2023-02-06 23:38:15 +0545

    add contracts

---
+++ Reentrancy/contracts/Wallet.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.17;
+
+contract Wallet {
+    mapping(address => uint256) public balanceOf;
+
+    function deposit() external payable {
+        balanceOf[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+       payable(msg.sender).transfer(balanceOf[msg.sender]);
+        balanceOf[msg.sender] = 0;
+    }
+}
